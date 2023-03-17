097a33bd4cfae6a2e892278c0c3ddab13c44ad45
luizfelixxx <Luiz.coffex@gmail.com>
2023-03-17 12:09:08 -0300

    Exercício Aula 1
    Método Duck

--- dino_runner/components/dinosaur.py
+++ dino_runner/components/dinosaur.py
@@ -1,8 +1,9 @@
-from dino_runner.utils.constants import RUNNING, JUMPING
+from dino_runner.utils.constants import RUNNING, JUMPING, DUCKING
 import pygame
 
 X_POS = 80
 Y_POS = 310
+Y_POS_DUCK = 340
 JUMP_VEL = 8.5
 
 class Dinosaur:
@@ -13,9 +14,10 @@ class Dinosaur:
         self.dino_rect.y = Y_POS
         self.step_index = 0
         self.jump_vel = JUMP_VEL
+       
         self.dino_jump = False
         self.dino_run = True
-
+        self.dino_duck = False
 
     def update(self, user_input):
         if self.dino_run:
@@ -24,11 +26,21 @@ class Dinosaur:
         if self.dino_jump:
             self.jump()
         
+        if self.dino_duck: 
+            self.duck()
+        
         if user_input[pygame.K_UP] and not self.dino_jump:
             self.dino_jump = True
             self.dino_run = False
+            self.dino_duck = False
         
-        elif not self.dino_jump:
+        elif user_input[pygame.K_DOWN] and not self.dino_jump:
+            self.dino_duck = True
+            self.dino_run = False
+            self.dino_jump = False
+        
+        elif not (self.dino_jump or user_input[pygame.K_UP]):
+            self.dino_duck = False
             self.dino_jump = False
             self.dino_run = True
 
@@ -57,4 +69,8 @@ class Dinosaur:
             self.jump_vel = JUMP_VEL
     
     def duck(self):
-        pass
+        self.image = DUCKING[0] if self.step_index < 5 else DUCKING[1]
+        self.dino_rect = self.image.get_rect()
+        self.dino_rect.x = X_POS
+        self.dino_rect.y = Y_POS_DUCK
+        self.step_index += 1
--- dino_runner/components/game.py
+++ dino_runner/components/game.py
@@ -1,7 +1,7 @@
 import pygame
 
 from dino_runner.utils.constants import BG, ICON, SCREEN_HEIGHT, SCREEN_WIDTH, TITLE, FPS
-from dino_runner.components.dinossauro import Dinosaur
+from dino_runner.components.dinosaur import Dinosaur
 
 
 class Game:
